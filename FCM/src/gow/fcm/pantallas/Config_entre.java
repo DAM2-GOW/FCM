eb9517773b0d3ff2a6a488337c16d8ffd080fef3
dai <dai@winmac13.alumnado.flo>
2014-02-13 19:44:37 +0100

    He colocado en mi clase los elementos que hay que rellenar con los datos necesarios de la base de datos

--- FCM/src/gow/fcm/pantallas/Config_entre.java
+++ FCM/src/gow/fcm/pantallas/Config_entre.java
@@ -1,16 +1,29 @@
 package gow.fcm.pantallas;
 
 import gow.fcm.footballcoachmanager.R;
+import gow.fcm.utilidades.ArrayAdapterStatsList;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class Config_entre extends Activity {
+ArrayAdapterStatsList aasl;
 
-	@Override
+@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_config_entre);
+		
+		ListView lv = (ListView)findViewById(R.id.listView1); 
+		
+		//Aqui llamo a la base de datos y obtengo datos.
+		
+		/*
+		aasl = new ArrayAdapterStatsList(context, titulo, tipo, datoEspecial)*/
+		
+		
+
 	}
 
 	@Override
--- FCM/src/gow/fcm/utilidades/ArrayAdapterStatsList.java
+++ FCM/src/gow/fcm/utilidades/ArrayAdapterStatsList.java
@@ -11,22 +11,18 @@ import android.widget.TextView;
 public class ArrayAdapterStatsList extends ArrayAdapter<String> {
   private final Context context; // Recoge el contexto en el que es lanzado.
   private final String[] titulo; // Recoge los nombres recibidos como array de String.
-  private final String[] type; // Recoge los tipos de jugador recibidos como array de String.
+  private final String[] tipo; // Recoge los tipos de jugador recibidos como array de String.
   private final String[] datoEspecial; // Recoge los numeros de cada jugador recibido como array de int.
 
   /**
    * Este es el constructor de la clase personalizada.
-   * Recibe los datos de nombre, tipo y numero de cada jugador.
-   * @param context
-   * @param name
-   * @param type
-   * @param num
+   * Recibe los datos de los entrenamientos
    */
-  public ArrayAdapterStatsList(Context context, String[] titulo, String[] type, String[] datoEspecial) {
+  public ArrayAdapterStatsList(Context context, String[] titulo, String[] tipo, String[] datoEspecial) {
     super(context, R.layout.activity_config_entre, titulo);
     this.context = context;
     this.titulo = titulo;
-    this.type = type;
+    this.tipo = tipo;
     this.datoEspecial = datoEspecial;
   }
 
@@ -40,15 +36,15 @@ public class ArrayAdapterStatsList extends ArrayAdapter<String> {
     View rowView = inflater.inflate(R.layout.activity_config_entre, parent, false);
     
     // Obtenemos los elementos de ese layout.
-    TextView tv_player_name = (TextView) rowView.findViewById(R.id.firstLine);
-    TextView tv_player_type = (TextView) rowView.findViewById(R.id.secondLine);
-    TextView tv_player_num = (TextView) rowView.findViewById(R.id.titulo);
+    TextView TV_dato_especial = (TextView) rowView.findViewById(R.id.firstLine);
+    TextView TV_types = (TextView) rowView.findViewById(R.id.secondLine);
+    TextView TV_titulo = (TextView) rowView.findViewById(R.id.titulo);
     
     //Rellenamos los elementos de ese layout con los elementos aportados
     // al llamar a esta clase.
-    tv_player_name.setText(titulo[position]);
-    tv_player_type.setText(type[position]);
-    tv_player_num.setText(datoEspecial[position]);
+    TV_dato_especial.setText(datoEspecial[position]);
+    TV_types.setText(tipo[position]);
+    TV_titulo.setText(titulo[position]);
     
     // Al ser una clase independiente, lo que nos interesa es obtener de ella datos,
     // ahora que han sido procesados las acciones oportunas devolvemos todos esos
