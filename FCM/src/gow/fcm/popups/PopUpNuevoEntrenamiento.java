982ba01868ec519bc924f7aea738e26753ff28f3
sergio <sergio@sergio-PC>
2014-02-13 20:05:11 +0100

    Esto es una prueba de fuego!!!

--- FCM/src/gow/fcm/popups/PopUpNuevoEntrenamiento.java
+++ FCM/src/gow/fcm/popups/PopUpNuevoEntrenamiento.java
@@ -3,16 +3,29 @@ package gow.fcm.popups;
 import gow.fcm.footballcoachmanager.R;
 import gow.fcm.footballcoachmanager.R.layout;
 import gow.fcm.footballcoachmanager.R.menu;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.widget.DatePicker;
 
 public class PopUpNuevoEntrenamiento extends Activity {
 
+	DatePicker dp;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		showAsPopup(this);
 		setContentView(R.layout.activity_popup_nuevo_entrenamiento);
+		
+		dp = (DatePicker) findViewById(R.id.datePicker1);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+	        dp.setCalendarViewShown(false);
+	    }
 	}
 
 	@Override
@@ -20,6 +33,20 @@ public class PopUpNuevoEntrenamiento extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.pop_up_nuevo_entrenamiento, menu);
 		return true;
+	}
+	
+	public static void showAsPopup(Activity activity) {
+		activity.requestWindowFeature(Window.FEATURE_ACTION_BAR); // Esta caracteristica habilita la barra (menu) en el Pop-Up.
+		activity.getWindow().setFlags(
+				WindowManager.LayoutParams.FLAG_DIM_BEHIND,
+				WindowManager.LayoutParams.FLAG_DIM_BEHIND); // Marcamos activity para habilitar opciones diversas.
+		LayoutParams params = activity.getWindow().getAttributes(); // Obtenemos objeto de configuracion del Activity.
+		params.height = LayoutParams.WRAP_CONTENT; // Adaptamos el tamaño en altura segun componentes del XML.
+		params.width = 800; // Fijamos el tamaño en anchura.
+		params.alpha = 1.0f; // Podemos otorgarle transparencia al Pop-Up.
+		params.dimAmount = 0.5f; // Fijamos el nivel de oscuridad para el activity de fondo.
+		activity.getWindow().setAttributes(
+				(android.view.WindowManager.LayoutParams) params); // Aplicamos los valores establecidos al Activity.
 	}
 
 }
